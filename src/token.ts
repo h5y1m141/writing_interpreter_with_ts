f0bcb0dcb2937357abccdc3adb91583d1f9f5955
HiroshiOyamada <h5y1m141+github@gmail.com>
2025-02-05 18:24:30 +0900

    任意のキーワードも識別してトークン生成できるように字句解析を修正

--- src/token.ts
+++ src/token.ts
@@ -9,6 +9,10 @@ export class Token {
   static ILLEGAL: TokenType = 'ILLEGAL'
   static EOF: TokenType = 'EOF'
 
+  // Identifiers + literals
+  static IDENT: TokenType = 'IDENT'
+  static INT: TokenType = 'INT'
+
   // Operators
   static ASSIGN: TokenType = '='
   static PLUS: TokenType = '+'
@@ -21,4 +25,27 @@ export class Token {
   static RPAREN: TokenType = ')'
   static LBRACE: TokenType = '{'
   static RBRACE: TokenType = '}'
+
+  // Keywords
+  static LET: TokenType = 'let'
+  static FUNCTION: TokenType = 'FUNCTION'
+  static TRUE: TokenType = 'TRUE'
+  static FALSE: TokenType = 'FALSE'
+  static IF: TokenType = 'IF'
+  static ELSE: TokenType = 'ELSE'
+  static RETURN: TokenType = 'RETURN'
+
+  static keywords: { [ident: string]: TokenType } = {
+    fn: Token.FUNCTION,
+    let: Token.LET,
+    true: Token.TRUE,
+    false: Token.FALSE,
+    if: Token.IF,
+    else: Token.ELSE,
+    return: Token.RETURN,
+  }
+
+  static lookupIdent(ident: string): TokenType {
+    return Token.keywords[ident] || Token.IDENT
+  }
 }
--- tests/lexer.test.ts
+++ tests/lexer.test.ts
@@ -5,15 +5,47 @@ import { Token } from 'src/token'
 
 describe('Lexer', () => {
   it('should tokenize input correctly', () => {
-    const input = `=+(){},;`
+    const input = `let five = 5; let ten = 10;
+    let add = fn(x, y) { x + y;
+    };
+    let result = add(five, ten);
+    `
     const tests: { expectedType: TokenType; expectedLiteral: string }[] = [
+      { expectedType: Token.LET, expectedLiteral: 'let' },
+      { expectedType: Token.IDENT, expectedLiteral: 'five' },
       { expectedType: Token.ASSIGN, expectedLiteral: '=' },
-      { expectedType: Token.PLUS, expectedLiteral: '+' },
+      { expectedType: Token.INT, expectedLiteral: '5' },
+      { expectedType: Token.SEMICOLON, expectedLiteral: ';' },
+      { expectedType: Token.LET, expectedLiteral: 'let' },
+      { expectedType: Token.IDENT, expectedLiteral: 'ten' },
+      { expectedType: Token.ASSIGN, expectedLiteral: '=' },
+      { expectedType: Token.INT, expectedLiteral: '10' },
+      { expectedType: Token.SEMICOLON, expectedLiteral: ';' },
+      { expectedType: Token.LET, expectedLiteral: 'let' },
+      { expectedType: Token.IDENT, expectedLiteral: 'add' },
+      { expectedType: Token.ASSIGN, expectedLiteral: '=' },
+      { expectedType: Token.FUNCTION, expectedLiteral: 'fn' },
       { expectedType: Token.LPAREN, expectedLiteral: '(' },
+      { expectedType: Token.IDENT, expectedLiteral: 'x' },
+      { expectedType: Token.COMMA, expectedLiteral: ',' },
+      { expectedType: Token.IDENT, expectedLiteral: 'y' },
       { expectedType: Token.RPAREN, expectedLiteral: ')' },
       { expectedType: Token.LBRACE, expectedLiteral: '{' },
+      { expectedType: Token.IDENT, expectedLiteral: 'x' },
+      { expectedType: Token.PLUS, expectedLiteral: '+' },
+      { expectedType: Token.IDENT, expectedLiteral: 'y' },
+      { expectedType: Token.SEMICOLON, expectedLiteral: ';' },
       { expectedType: Token.RBRACE, expectedLiteral: '}' },
+      { expectedType: Token.SEMICOLON, expectedLiteral: ';' },
+      { expectedType: Token.LET, expectedLiteral: 'let' },
+      { expectedType: Token.IDENT, expectedLiteral: 'result' },
+      { expectedType: Token.ASSIGN, expectedLiteral: '=' },
+      { expectedType: Token.IDENT, expectedLiteral: 'add' },
+      { expectedType: Token.LPAREN, expectedLiteral: '(' },
+      { expectedType: Token.IDENT, expectedLiteral: 'five' },
       { expectedType: Token.COMMA, expectedLiteral: ',' },
+      { expectedType: Token.IDENT, expectedLiteral: 'ten' },
+      { expectedType: Token.RPAREN, expectedLiteral: ')' },
       { expectedType: Token.SEMICOLON, expectedLiteral: ';' },
       { expectedType: Token.EOF, expectedLiteral: '' },
     ]
